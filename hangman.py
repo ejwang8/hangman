1aee83b1403f70668ca94b486a1e781729d5c65c
elizabethjwang <elizabethwang2001@gmail.com>
2023-11-21 09:50:18 -0800

    added to hangman

--- hangman.py
+++ hangman.py
@@ -1,2 +1,12 @@
-print("hello world")
-print("welcome to hangman")
+from importlib.machinery import WindowsRegistryFinder
+import random
+
+print("Welcome to Hangman")
+response = input("Would you like to play? (y/n)")
+if response != 'y':
+    quit()
+
+words = ["bear", "adieu", "wordle", "hello", "wordle", "fear the tree"]
+randomIndex = random.randint(1, len(words)) - 1
+word = words[randomIndex]
+print(word)
